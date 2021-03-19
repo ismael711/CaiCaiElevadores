c3dc961b43fec4f3158a143a0c5c44d009e22fd1
Ismael Rojas <ismael711@gmail.com>
2021-03-19 18:45:17 -0300

    Jaxa Xpert - Módulo 2 - POO - Aula 02

---
+++ src/br/com/caicai/teste/TesteElevador.java
@@ -0,0 +1,22 @@
+package br.com.caicai.teste;
+
+import br.com.caicai.modelo.Elevador;
+
+public class TesteElevador {
+
+	public static void main(String[] args) {
+		
+		Elevador objeto = new Elevador(); //instanciando
+		objeto.inicializar("Torre A",  20, 10, 0);
+		System.out.println(objeto.toString());
+		objeto.entrar();
+		objeto.entrar();
+		objeto.subir(2);
+		System.out.println(objeto.toString());
+		objeto.descer();
+		objeto.sair();
+		System.out.println(objeto.toString());
+
+	}
+
+}
